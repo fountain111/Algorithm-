4e80ef6631c1da0a9f550341f7ea46aaf9318ddf
kakurong <86200528@qq.com>
2018-08-28 17:28:21 +0800

    TEST

--- sort.py
+++ sort.py
@@ -1,5 +1,16 @@
 import numpy as np
-import random
+from flask import Flask
+import  pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
+from flask import render_template
+import sys
+from flask import make_response
+import math
+app = Flask(__name__)
+plt.switch_backend('agg') # solve main loop
 class Sort():
     def insertion_sort(self,list_):
         j = 1
@@ -15,12 +26,70 @@ class Sort():
         return list_
 
 
-    list_ = [random.randint(1,9) for i in range(1,10)]
 
 
 
-print(list(range(1,100)))
 
 
 #def merge_sort():
-    
+
+def generate_point(start,end):
+
+    rand_x_y  = [np.random.randint(start,end) for  i in range(1,3)]
+
+    return rand_x_y
+
+def generate_line(x_start,x_end,slope,b):
+
+    x_list = []
+    y_list = []
+    for i in range(1,100):
+        x = np.random.randint(x_start,x_end)
+        y = x *slope+b
+        x_list.append(x)
+        y_list.append(y)
+    return x_list,y_list
+
+
+
+def plot_margins(margin_name):
+    normal_vector = [-2, 1]
+    slope = -normal_vector[0]
+    b = 0
+    normal_length = 0
+    for value in normal_vector:
+        normal_length += value*value
+    normal_unit_vector = np.array(normal_vector)/normal_length
+
+    line_x_y = generate_line(-200,200,slope,b)
+
+    for i in range(1, 5):
+        rand_x_y = generate_point(-300,300)
+
+        if margin_name == 'functional_margins':
+            margin = np.dot(normal_vector,np.array(rand_x_y).T)+b
+
+
+        elif margin_name == 'geometric_margins':
+            margin = np.dot(normal_unit_vector ,np.array(rand_x_y).T) + b/ normal_length
+        print(margin)
+        if margin >0:
+            marker = 'D'
+        else:
+            marker = 'o'
+        plt.scatter(x=rand_x_y[0],y=rand_x_y[1], marker=marker)
+
+        if margin > 0:
+            margin *= 1
+        else:
+            margin *= -1
+        plt.annotate('fun_r={margin}'.format(margin=margin), rand_x_y)
+
+    plt.plot(line_x_y[0],line_x_y[1])
+
+    plt.title(margin_name)
+    plt.savefig('./static/{fun_name}.png'.format(fun_name=margin_name))
+    plt.savefig('./static/test.png')
+    plt.clf()
+    return 0
+plot_margins('geometric_margins')
--- svm.py
+++ svm.py
@@ -9,63 +9,76 @@ import sys
 from flask import make_response
 import math
 app = Flask(__name__)
+plt.switch_backend('agg') # solve main loop
 
-def generate_point(slope,b):
-    rand_x = np.random.randint(1, 10)
-    functional_margin = np.random.randint(-100, 100)
-    rand_y = rand_x * slope + functional_margin + b
-    if rand_y > b:
-        mark = 'D'
-    else:
-        mark = 'o'
 
-    return rand_x,rand_y,{'mark':mark},functional_margin
+def generate_point(start,end):
+
+    rand_x_y  = [np.random.randint(start,end) for  i in range(1,3)]
+
+    return rand_x_y
+
+def generate_line(x_start,x_end,slope,b):
+
+    x_list = []
+    y_list = []
+    for i in range(1,100):
+        x = np.random.randint(x_start,x_end)
+        y = x *slope+b
+        x_list.append(x)
+        y_list.append(y)
+    return x_list,y_list
+
+
+
+def plot_margins(margin_name):
+    normal_vector = [-2, 1]
+    slope = -normal_vector[0]
+    b = 0
+    normal_length = 0
+    for value in normal_vector:
+        normal_length += value*value
+    normal_unit_vector = np.array(normal_vector)/normal_length
+
+    line_x_y = generate_line(-200,200,slope,b)
+
+    for i in range(1, 5):
+        rand_x_y = generate_point(-300,300)
+
+        if margin_name == 'functional_margins':
+            margin = np.dot(normal_vector,np.array(rand_x_y).T)+b
+
+
+        elif margin_name == 'geometric_margins':
+            margin = np.dot(normal_unit_vector ,np.array(rand_x_y).T) + b/ normal_length
+        print(margin)
+        if margin >0:
+            marker = 'D'
+        else:
+            marker = 'o'
+        plt.scatter(x=rand_x_y[0],y=rand_x_y[1], marker=marker)
+
+        if margin > 0:
+            margin *= 1
+        else:
+            margin *= -1
+        plt.annotate('fun_r={margin}'.format(margin=margin), rand_x_y)
+
+    plt.plot(line_x_y[0],line_x_y[1])
+
+    plt.title(margin_name)
+    plt.savefig('./static/{fun_name}.png'.format(fun_name=margin_name))
+    plt.savefig('./static/test.png')
+    plt.clf()
+    return 0
 
 @app.route('/functional_margins')
 def functional_margin():
-    fun_name = sys._getframe().f_code.co_name
-
-    x = np.array(range(1, 10))
-    slope = 2
-    b = 0
-    y = x*(slope)+b
-    normal_vector = [2,-1]
-    plt.plot(x, y)
-    for i in range(1,10):
-        rand_x,rand_y,mark,functional_margin = generate_point(slope,b)
-        plt.scatter(rand_x, rand_y, marker=mark.mark)
-
-        if functional_margin > 0:
-            functional_margin *= 1
-        else:
-            functional_margin *= -1
-        plt.annotate('fun_r={margin}'.format(margin=functional_margin), [rand_x, rand_y])
-
-    plt.title(fun_name+'_no_sclar')
-    plt.savefig('./static/{fun_name}.png'.format(fun_name=fun_name))
-    plt.savefig('./static/test.png')
-    plt.clf()
+    plot_margins('functional_margins')
     return render_template('test_plot.html')
 
 @app.route('/geometric_margins')
 def gemotric_margins():
-    fun_name = sys._getframe().f_code.co_name
-    b = 5
-    normal_vector = [2,-1]
-    normal_length = 0
-    for value in normal_vector:
-        normal_length += math.sqrt(value)
-    unit_vector = normal_vector/normal_length
-    for i in range(1,10):
-        x,y,functional_margin,mark = generate_point(normal_vector[0],b)
-        gemotric_margins = np.array(normal_vector/unit_vector).transpose()*x +b/unit_vector
-        if gemotric_margins > 0:
-            gemotric_margins *= 1
-        else:
-            gemotric_margins *= -1
-        plt.annotate('fun_r={margin}'.format(margin=gemotric_margins), [x, y])
-        plt.title(fun_name)
-        plt.savefig('./static/{fun_name}.png'.format(fun_name=fun_name))
-        plt.savefig('./static/test.png')
-        plt.clf()
+    plot_margins('geometric_margins')
     return render_template('test_plot.html')
+
