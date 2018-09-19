8d650c93359caa63a054f34b52ac4456bba7fcde
fountain111 <fountainoy@icloud.com>
2018-09-20 00:40:25 +0800

    8.26

--- perceptron.py
+++ perceptron.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 def generate_samples(start,end,size):
     samples = []
-    w = np.array([2,2])
+    w = np.array([1,1])
     b = 3
 
     for i in range(size):
@@ -18,7 +18,7 @@ def generate_samples(start,end,size):
     return samples
 
 # w=<2,2> b = 3
-samples = generate_samples(-300,300,100)
+samples = generate_samples(-300,300,5)
 print(samples)
 
 def plot_samples():
@@ -34,22 +34,29 @@ def plot_samples():
 
     plt.title('samples')
     plt.savefig('./static/test.png')
-
+plot_samples()
 def perceptron(samples):
-    w = np.array([1.0,2.0])
+    w = np.array([0.0,0.0])
     b = 1.0
-    lr = 0.1
+    lr = 0.5
+    epoch = 1000000
+    for i in range(epoch):
 
+        for value in samples:
+            x1= value[0][0]
+            x2 =value[0][1]
+            y = value[1]
+            predict = w[0]*x1+w[1]*x2+b
+            if predict >0:
+                predict = 1
+            else:
+                predict=-1
+            if (y*predict<=0):
+                w[0] = w[0] +lr*y*x1
+                w[1] = w[1]+lr*y*x2
+                b= b + lr*y
+                print(w,b)
 
-
-    for value in samples:
-        x= value[0]
-        y = value[1]
-
-        if (y*(w.dot(x)+b)<=0):
-            w+=lr*np.array(x)
-            b+=lr
-            print(w,b)
 
     return w,b
 print(perceptron(samples))
